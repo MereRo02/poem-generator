62c741eee49593cb63a3e3b10536c00da28e2361
MereRo02 <roudeenr@gmail.com>
2025-07-30 21:29:57 +0200

    javascript added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import "./styles.css";
+function generatePoem(event) {
+	event.preventDefault();
+	alert("Generating your poem...");
+
+let poemElement = document.querySelector("#poem");
+poemElement.innerHTML = "La tombe dit a la rose"; 
+}
+
+let poemFormElement = document.querySelector("poem-generator");
+poemFormElement.addEventListener("submit", generatePoem); 
